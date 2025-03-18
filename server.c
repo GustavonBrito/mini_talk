17dc77394954642e4f2f725ef3d287d8f120ada5
GustavonBrito <105254086+GustavonBrito@users.noreply.github.com>
2025-03-18 17:51:58 +0000

    feat: server being implemented

--- server.c
+++ server.c
@@ -6,15 +6,18 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:34:41 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/03/18 13:15:33 by codespace        ###   ########.fr       */
+/*   Updated: 2025/03/18 17:33:52 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handle_sigusr1(int sig)
+void handle_signal(int sig)
 {
-	write(1, "Sinal recebido\n", 15);
+	if (sig == 10)
+		write(1, "1", 1);
+	else
+		write(1, "0", 1);	
 }
 
 int main(void)
@@ -25,9 +28,10 @@ int main(void)
 	pid = getpid();
 	printf("\033[94mPID\033[0m \033[96m->\033[0m %d\n", pid);
 	printf("\033[90mWaiting for a message...\033[0m\n");
-	signal(SIGUSR1, handle_sigusr1);
-	signal(SIGUSR2, handle_sigusr1);
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
 	while (1)
 		pause();
+	write(1, "\n", 1);
 	return (0);
 }

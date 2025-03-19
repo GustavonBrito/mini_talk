9f0bc1a60ad132b41af82aad345fc17fdb440533
GustavonBrito <gustavoserafio@gmail.com>
2025-03-19 15:57:40 -0300

    feat: client

--- client.c
+++ client.c
@@ -3,50 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:34:46 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/03/18 18:18:06 by codespace        ###   ########.fr       */
+/*   Updated: 2025/03/19 15:50:37 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void post_datas(int pid, char *message)
+void	post_datas(int pid, char *message)
 {
-    int bit_per_bit;
-    int i;
+	int	bit_per_bit;
+	int	i;
 
-    bit_per_bit = 7;
-    i = 0;
-    while (message[i])
-    {
-        bit_per_bit = 7;
-        while(bit_per_bit >= 0)
-        {
-            if (message[i] >> bit_per_bit & 1)
-            {
-                kill(pid, SIGUSR1);
-            }
-            else
-            { 
-                kill(pid, SIGUSR2);    
-            }
-            bit_per_bit--;
-            usleep(100);
-        }
-        i++;
-    }
+	bit_per_bit = 7;
+	i = 0;
+	while (message[i])
+	{
+		bit_per_bit = 7;
+		while (bit_per_bit >= 0)
+		{
+			if (message[i] >> bit_per_bit & 1)
+			{
+				kill(pid, SIGUSR1);
+			}
+			else
+			{
+				kill(pid, SIGUSR2);
+			}
+			bit_per_bit--;
+			usleep(100);
+		}
+		usleep(100);
+		i++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        printf("\033[31mBad Request: Insert 3 args, eg: ./client <PID> <Message>...\033[31m\n");
-        return (-1);
-    }
-
-    post_datas(atoi(argv[1]), argv[2]);
-    return (0);
+	if (argc != 3)
+	{
+		printf("\033[31mInsert 3 args: ./client <PID> <Message>\033[31m\n");
+		return (-1);
+	}
+	post_datas(atoi(argv[1]), argv[2]);
+	return (0);
 }

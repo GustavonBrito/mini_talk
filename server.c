b8e2b3d1bdc8a61cdc35759d885c74b2e601ac56
GustavonBrito <gustavoserafio@gmail.com>
2025-03-20 09:40:48 -0300

    feat: normalized to norminette and update header path

--- server.c
+++ server.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:34:41 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/03/18 19:51:03 by codespace        ###   ########.fr       */
+/*   Updated: 2025/03/19 23:12:04 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "libft/headers/minitalk.h"
 
-void handle_signal(int sig)
+void	handle_signal(int sig)
 {
-	int i;
-	unsigned char g;
-	static int bit_count = 0;
-	static char byte_to_print[8];
-	
+	int				i;
+	unsigned char	g;
+	static int		bit_count = 0;
+	static char		byte_to_print[8];
+
 	if (sig == 10)
 		byte_to_print[bit_count] = '1';
 	else
@@ -28,7 +28,7 @@ void handle_signal(int sig)
 	{
 		byte_to_print[bit_count] = '\0';
 		i = 0;
-		while(i < 8)
+		while (i < 8)
 		{
 			g = (g << 1) | (byte_to_print[i] - '0');
 			i++;
@@ -39,17 +39,18 @@ void handle_signal(int sig)
 	}
 }
 
-int main(void)
+int	main(void)
 {
-	int pid;
-	
+	int	pid;
+
 	pid = 0;
 	pid = getpid();
-	printf("\033[94mPID\033[0m \033[96m->\033[0m %d\n", pid);
-	printf("\033[90mWaiting for a message...\033[0m\n");
+	ft_printf("\033[94mPID\033[0m \033[96m->\033[0m %d\n", pid);
+	ft_printf("\033[90mWaiting for a message...\033[0m\n");
 	signal(SIGUSR1, handle_signal);
 	signal(SIGUSR2, handle_signal);
 	while (1)
 		pause();
+	write(1, "\n", 1);
 	return (0);
 }
